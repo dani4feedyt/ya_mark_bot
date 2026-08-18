3acdd61a6365ac94872601d4c069b0f003800a4c
dani4feed <dan.arbatov@ukr.net>
2026-08-18 15:11:54 +0300

    release 2.1.6.4 -fix: removed impersonation

--- main.py
+++ main.py
@@ -385,10 +385,6 @@ def load_video(url, shortcode):
         'recode_video': 'mp4',
         'socket_timeout': 20,
         'retries': 3,
-        'http_headers': {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36',
-        },
-        'impersonate': 'chrome',
     }
 
     info = probe_video(url, base_opts)
